185cddb4d3a6899401d998c5c19a2bd036702fe2
Ivan Gagarinov <dzencot@gmail.com>
2017-01-04 22:51:10 +0500

    Changes to be committed: 	modified:   bin/brain-even.js 	modified:   package.json

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
 // @flow
 
-console.log('Welcome to the Brain Games!');
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!\n');
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, '${name}'!`);
